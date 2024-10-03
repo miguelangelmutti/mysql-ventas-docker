ccf3dd209eae5b3cd536417f6bfe620403d49c0c
miguem89 <miguelangelmutti@hotmail.com>
2024-10-03 19:46:45 -0300

    agrego sql script

---
+++ Script.sql
@@ -0,0 +1,518 @@
+-- Parte de Creación y carga
+DROP DATABASE base_negocio;
+CREATE DATABASE IF NOT EXISTS base_negocio;
+USE base_negocio;
+
+-- SELECT @@global.secure_file_priv; -- Para ver la ruta de origen donde poner los csv.
+
+-- C:\\wamp64\\tmp\\
+-- C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\
+
+
+/*Importacion de las tablas*/
+DROP TABLE IF EXISTS `gasto`;
+CREATE TABLE IF NOT EXISTS `gasto` (
+  	`IdGasto` 		INTEGER,
+  	`IdSucursal` 	INTEGER,
+  	`IdTipoGasto` 	INTEGER,
+    `Fecha`			DATE,
+  	`Monto` 		DECIMAL(10,2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;	
+
+LOAD DATA INFILE '/var/lib/mysql-files/Gasto.csv'
+INTO TABLE `gasto` 
+FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '' 
+LINES TERMINATED BY '\n' IGNORE 1
+LINES (IdGasto,IdSucursal,IdTipoGasto,Fecha,Monto);
+-- SELECT * FROM gasto;
+
+DROP TABLE IF EXISTS `compra`;
+CREATE TABLE IF NOT EXISTS `compra` (
+  `IdCompra`			INTEGER,
+  `Fecha` 				DATE,
+  `IdProducto`			INTEGER,
+  `Cantidad`			INTEGER,
+  `Precio`				DECIMAL(10,2),
+  `IdProveedor`			INTEGER
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+LOAD DATA INFILE '/var/lib/mysql-files/Compra.csv' 
+INTO TABLE `compra` 
+FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM compra;
+
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE IF NOT EXISTS `venta` (
+  `IdVenta`				INTEGER,
+  `Fecha` 				DATE NOT NULL,
+  `Fecha_Entrega` 		DATE NOT NULL,
+  `IdCanal`				INTEGER, 
+  `IdCliente`			INTEGER, 
+  `IdSucursal`			INTEGER,
+  `IdEmpleado`			INTEGER,
+  `IdProducto`			INTEGER,
+  `Precio`				VARCHAR(30),
+  `Cantidad`			VARCHAR(30)
+  -- `Precio`			DECIMAL(10,2),
+  -- `Cantidad`			INTEGER
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+LOAD DATA INFILE '/var/lib/mysql-files/Venta.csv' 
+INTO TABLE `venta` 
+FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '' 
+LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+-- SELECT * FROM venta;
+
+DROP TABLE IF EXISTS sucursal;
+CREATE TABLE IF NOT EXISTS sucursal (
+	ID			INTEGER,
+	Sucursal	VARCHAR(40),
+	Domicilio	VARCHAR(150),
+	Localidad	VARCHAR(80),
+	Provincia	VARCHAR(50),
+	Latitud2	VARCHAR(30),
+	Longitud2	VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE '/var/lib/mysql-files/Sucursales.csv' 
+INTO TABLE sucursal
+CHARACTER SET latin1 -- Si no colocamos esta línea, no reconoce la codificación adecuada ANSI
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM sucursal;
+-- TRUNCATE TABLE sucursal;
+
+LOAD DATA INFILE '/var/lib/mysql-files/Sucursales.csv' 
+INTO TABLE sucursal
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM sucursal;
+
+DROP TABLE IF EXISTS cliente;
+CREATE TABLE IF NOT EXISTS cliente (
+	ID					INTEGER,
+	Provincia			VARCHAR(50),
+	Nombre_y_Apellido	VARCHAR(80),
+	Domicilio			VARCHAR(150),
+	Telefono			VARCHAR(30),
+	Edad				VARCHAR(5),
+	Localidad			VARCHAR(80),
+	X					VARCHAR(30),
+	Y					VARCHAR(30),
+    Fecha_Alta			DATE NOT NULL,
+    Usuario_Alta		VARCHAR(20),
+    Fecha_Ultima_Modificacion		DATE NOT NULL,
+    Usuario_Ultima_Modificacion		VARCHAR(20),
+    Marca_Baja			TINYINT,
+	col10				VARCHAR(1)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+TRUNCATE TABLE base_negocio.cliente
+LOAD DATA INFILE '/var/lib/mysql-files/Clientes.csv'
+INTO TABLE base_negocio.cliente
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM cliente;
+
+DROP TABLE IF EXISTS `canal_venta`;
+CREATE TABLE IF NOT EXISTS `canal_venta` (
+  `IdCanal`				INTEGER,
+  `Canal` 				VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+/*TRUNCATE TABLE canal_venta*/
+
+LOAD DATA INFILE '/var/lib/mysql-files/CanalDeVenta.csv' 
+INTO TABLE `canal_venta` 
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"'
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM canal_venta;
+
+DROP TABLE IF EXISTS `tipo_gasto`;
+CREATE TABLE IF NOT EXISTS `tipo_gasto` (
+  `IdTipoGasto` int(11) NOT NULL AUTO_INCREMENT,
+  `Descripcion` varchar(100) NOT NULL,
+  `Monto_Aproximado` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`IdTipoGasto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE '/var/lib/mysql-files/TiposDeGasto.csv' 
+INTO TABLE `tipo_gasto` 
+FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM tipo_gasto;
+
+DROP TABLE IF EXISTS proveedor;
+CREATE TABLE IF NOT EXISTS proveedor (
+	IDProveedor		INTEGER,
+	Nombre			VARCHAR(80),
+	Domicilio		VARCHAR(150),
+	Ciudad			VARCHAR(80),
+	Provincia		VARCHAR(50),
+	Pais			VARCHAR(20),
+	Departamento	VARCHAR(80)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE '/var/lib/mysql-files/Proveedores.csv' 
+INTO TABLE proveedor
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM proveedor;
+
+DROP TABLE IF EXISTS producto;
+CREATE TABLE IF NOT EXISTS producto (
+	IDProducto					INTEGER,
+	Concepto					VARCHAR(100),
+	Tipo						VARCHAR(50),
+	Precio2						VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE '/var/lib/mysql-files/PRODUCTOS.csv' 
+INTO TABLE `producto` 
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM producto;
+
+DROP TABLE IF EXISTS empleado;
+CREATE TABLE IF NOT EXISTS empleado (
+	IDEmpleado					INTEGER,
+	Apellido					VARCHAR(100),
+	Nombre						VARCHAR(100),
+	Sucursal					VARCHAR(50),
+	Sector						VARCHAR(50),
+	Cargo						VARCHAR(50),
+	Salario2					VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE '/var/lib/mysql-files/Empleados.csv' 
+INTO TABLE `empleado` 
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;
+-- SELECT * FROM empleado;
+
+/*Nombres de campos*/
+use base_negocio;
+SET SQL_SAFE_UPDATES = 0;
+/*Función y Procedimiento provistos*/
+SET GLOBAL log_bin_trust_function_creators = 1;
+DROP FUNCTION IF EXISTS `UC_Words`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `UC_Words`( str VARCHAR(255) ) RETURNS varchar(255) CHARSET utf8
+BEGIN  
+  DECLARE c CHAR(1);  
+  DECLARE s VARCHAR(255);  
+  DECLARE i INT DEFAULT 1;  
+  DECLARE bool INT DEFAULT 1;  
+  DECLARE punct CHAR(17) DEFAULT ' ()[]{},.-_!@;:?/';  
+  SET s = LCASE( str );  
+  WHILE i < LENGTH( str ) DO  
+     BEGIN  
+       SET c = SUBSTRING( s, i, 1 );  
+       IF LOCATE( c, punct ) > 0 THEN  
+        SET bool = 1;  
+      ELSEIF bool=1 THEN  
+        BEGIN  
+          IF c >= 'a' AND c <= 'z' THEN  
+             BEGIN  
+               SET s = CONCAT(LEFT(s,i-1),UCASE(c),SUBSTRING(s,i+1));  
+               SET bool = 0;  
+             END;  
+           ELSEIF c >= '0' AND c <= '9' THEN  
+            SET bool = 0;  
+          END IF;  
+        END;  
+      END IF;  
+      SET i = i+1;  
+    END;  
+  END WHILE;  
+  RETURN s;  
+END$$
+DELIMITER ;
+DROP PROCEDURE IF EXISTS `Llenar_dimension_calendario`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Llenar_dimension_calendario`(IN `startdate` DATE, IN `stopdate` DATE)
+BEGIN
+    DECLARE currentdate DATE;
+    SET currentdate = startdate;
+    WHILE currentdate < stopdate DO
+        INSERT INTO calendario VALUES (
+                        YEAR(currentdate)*10000+MONTH(currentdate)*100 + DAY(currentdate),
+                        currentdate,
+                        YEAR(currentdate),
+                        MONTH(currentdate),
+                        DAY(currentdate),
+                        QUARTER(currentdate),
+                        WEEKOFYEAR(currentdate),
+                        DATE_FORMAT(currentdate,'%W'),
+                        DATE_FORMAT(currentdate,'%M'));
+        SET currentdate = ADDDATE(currentdate,INTERVAL 1 DAY);
+    END WHILE;
+END$$
+DELIMITER ;
+
+/*Se genera la dimension calendario*/
+DROP TABLE IF EXISTS `calendario`;
+CREATE TABLE calendario (
+        id                      INTEGER PRIMARY KEY,  -- year*10000+month*100+day
+        fecha                 	DATE NOT NULL,
+        anio                    INTEGER NOT NULL,
+        mes                   	INTEGER NOT NULL, -- 1 to 12
+        dia                     INTEGER NOT NULL, -- 1 to 31
+        trimestre               INTEGER NOT NULL, -- 1 to 4
+        semana                  INTEGER NOT NULL, -- 1 to 52/53
+        dia_nombre              VARCHAR(9) NOT NULL, -- 'Monday', 'Tuesday'...
+        mes_nombre              VARCHAR(9) NOT NULL -- 'January', 'February'...
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+/*LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Calendario.csv' 
+INTO TABLE calendario
+FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;*/
+
+/*TRUNCATE TABLE calendario;*/
+CALL Llenar_dimension_calendario('2015-01-01','2024-10-01');
+SELECT * FROM calendario;
+
+--###Resolución de la consigna
+ALTER TABLE `calendario` CHANGE `id` `IdFecha` INT(11) NOT NULL; -- Cambiamos Nombre a la columna id
+ALTER TABLE `calendario` ADD UNIQUE(`fecha`); -- Creamos un campo único en fecha
+
+/*Normalización tabla producto*/
+select * from producto;
+ALTER TABLE `producto` ADD `IdTipoProducto` INT NOT NULL DEFAULT '0' AFTER `Precio2`;
+
+DROP TABLE IF EXISTS `tipo_producto`;
+CREATE TABLE IF NOT EXISTS `tipo_producto` (
+  `IdTipoProducto` int(11) NOT NULL AUTO_INCREMENT,
+  `TipoProducto` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdTipoProducto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO tipo_producto (TipoProducto) SELECT DISTINCT Tipo FROM producto ORDER BY 1;
+
+select * from cliente;
+select * from sucursal;
+
+ALTER TABLE `cliente` CHANGE `ID` `IdCliente` INT(11) NOT NULL;
+ALTER TABLE `empleado` CHANGE `IDEmpleado` `IdEmpleado` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `proveedor` CHANGE `IDProveedor` `IdProveedor` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `sucursal` CHANGE `ID` `IdSucursal` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `tipo_gasto` CHANGE `Descripcion` `Tipo_Gasto` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL;
+ALTER TABLE `producto` CHANGE `IDProducto` `IdProducto` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `producto` CHANGE `Concepto` `Producto` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NULL DEFAULT NULL;
+
+
+/*Tipos de Datos*/
+ALTER TABLE `cliente` 	ADD `Latitud` DECIMAL(13,10) NOT NULL DEFAULT '0' AFTER `Y`, 
+						ADD `Longitud` DECIMAL(13,10) NOT NULL DEFAULT '0' AFTER `Latitud`;
+UPDATE cliente SET Y = '0' WHERE Y = '';
+UPDATE cliente SET X = '0' WHERE X = '';
+UPDATE `cliente` SET Latitud = REPLACE(Y,',','.');
+UPDATE `cliente` SET Longitud = REPLACE(X,',','.');
+-- SELECT * FROM `cliente`;
+ALTER TABLE `cliente` DROP `Y`;
+ALTER TABLE `cliente` DROP `X`;
+
+
+select * from `empleado`
+
+ALTER TABLE `empleado` ADD `Salario` DECIMAL(10,2) NOT NULL DEFAULT '0' AFTER `Salario2`;
+UPDATE `empleado` SET Salario = cast(replace(Salario2,',','.') as decimal(10,2));
+ALTER TABLE `empleado` DROP `Salario2`;
+
+ALTER TABLE `producto` ADD `Precio` DECIMAL(15,3) NOT NULL DEFAULT '0' AFTER `Precio2`;
+UPDATE `producto` SET Precio = REPLACE(Precio2,',','.');
+ALTER TABLE `producto` DROP `Precio2`;
+
+ALTER TABLE `sucursal` 	ADD `Latitud` DECIMAL(13,10) NOT NULL DEFAULT '0' AFTER `Longitud2`, 
+						ADD `Longitud` DECIMAL(13,10) NOT NULL DEFAULT '0' AFTER `Latitud`;
+UPDATE `sucursal` SET Latitud = REPLACE(Latitud2,',','.');
+UPDATE `sucursal` SET Longitud = REPLACE(Longitud2,',','.');
+-- SELECT * FROM `sucursal`;
+ALTER TABLE `sucursal` DROP `Latitud2`;
+ALTER TABLE `sucursal` DROP `Longitud2`;
+
+UPDATE `venta` set `Precio` = 0 WHERE `Precio` = '';
+ALTER TABLE `venta` CHANGE `Precio` `Precio` DECIMAL(15,3) NOT NULL DEFAULT '0';
+
+/*Columnas sin usar*/
+ALTER TABLE `cliente` DROP `col10`;
+
+-- SELECT * FROM cliente;
+
+/*Imputar Valores Faltantes*/
+UPDATE `cliente` SET Domicilio = 'Sin Dato' WHERE TRIM(Domicilio) = "" OR ISNULL(Domicilio);
+UPDATE `cliente` SET Localidad = 'Sin Dato' WHERE TRIM(Localidad) = "" OR ISNULL(Localidad);
+UPDATE `cliente` SET Nombre_y_Apellido = 'Sin Dato' WHERE TRIM(Nombre_y_Apellido) = "" OR ISNULL(Nombre_y_Apellido);
+UPDATE `cliente` SET Provincia = 'Sin Dato' WHERE TRIM(Provincia) = "" OR ISNULL(Provincia);
+
+UPDATE `empleado` SET Apellido = 'Sin Dato' WHERE TRIM(Apellido) = "" OR ISNULL(Apellido);
+UPDATE `empleado` SET Nombre = 'Sin Dato' WHERE TRIM(Nombre) = "" OR ISNULL(Nombre);
+UPDATE `empleado` SET Sucursal = 'Sin Dato' WHERE TRIM(Sucursal) = "" OR ISNULL(Sucursal);
+UPDATE `empleado` SET Sector = 'Sin Dato' WHERE TRIM(Sector) = "" OR ISNULL(Sector);
+UPDATE `empleado` SET Cargo = 'Sin Dato' WHERE TRIM(Cargo) = "" OR ISNULL(Cargo);
+
+UPDATE `producto` SET Producto = 'Sin Dato' WHERE TRIM(Producto) = "" OR ISNULL(Producto);
+UPDATE `producto` SET Tipo = 'Sin Dato' WHERE TRIM(Tipo) = "" OR ISNULL(Tipo);
+
+UPDATE `proveedor` SET Nombre = 'Sin Dato' WHERE TRIM(Nombre) = "" OR ISNULL(Nombre);
+UPDATE `proveedor` SET Domicilio = 'Sin Dato' WHERE TRIM(Domicilio) = "" OR ISNULL(Domicilio);
+UPDATE `proveedor` SET Ciudad = 'Sin Dato' WHERE TRIM(Ciudad) = "" OR ISNULL(Ciudad);
+UPDATE `proveedor` SET Provincia = 'Sin Dato' WHERE TRIM(Provincia) = "" OR ISNULL(Provincia);
+UPDATE `proveedor` SET Pais = 'Sin Dato' WHERE TRIM(Pais) = "" OR ISNULL(Pais);
+UPDATE `proveedor` SET Departamento = 'Sin Dato' WHERE TRIM(Departamento) = "" OR ISNULL(Departamento);
+
+UPDATE `sucursal` SET Domicilio = 'Sin Dato' WHERE TRIM(Domicilio) = "" OR ISNULL(Domicilio);
+UPDATE `sucursal` SET Sucursal = 'Sin Dato' WHERE TRIM(Sucursal) = "" OR ISNULL(Sucursal);
+UPDATE `sucursal` SET Provincia = 'Sin Dato' WHERE TRIM(Provincia) = "" OR ISNULL(Provincia);
+UPDATE `sucursal` SET Localidad = 'Sin Dato' WHERE TRIM(Localidad) = "" OR ISNULL(Localidad);
+
+/*Normalizacion a Letra Capital*/
+UPDATE cliente SET  Domicilio = UC_Words(TRIM(Domicilio)),
+                    Nombre_y_Apellido = UC_Words(TRIM(Nombre_y_Apellido));
+                    
+UPDATE cliente SET  Localidad = UC_Words(TRIM(Localidad));
+					
+UPDATE sucursal SET Domicilio = UC_Words(TRIM(Domicilio)),
+                    Sucursal = UC_Words(TRIM(Sucursal));
+					
+UPDATE proveedor SET Nombre = UC_Words(TRIM(Nombre)),
+                    Domicilio = UC_Words(TRIM(Domicilio));
+                    
+UPDATE proveedor SET Ciudad = UC_Words(TRIM(Ciudad)),
+                    Provincia = UC_Words(TRIM(Provincia));
+
+UPDATE producto SET Producto = UC_Words(TRIM(Producto));
+
+UPDATE tipo_producto SET TipoProducto = UC_Words(TRIM(TipoProducto));
+					
+UPDATE empleado SET Nombre = UC_Words(TRIM(Nombre)),
+                    Apellido = UC_Words(TRIM(Apellido));
+
+/*Tabla ventas limpieza y normalizacion*/
+/*
+select * from venta where Precio = '' or Cantidad = '';
+select count(*) from venta;
+*/
+UPDATE venta v JOIN producto p ON (v.IdProducto = p.IdProducto) 
+SET v.Precio = p.Precio
+WHERE v.Precio = 0;
+
+/*Tabla auxiliar donde se guardarán registros con problemas:
+1-Cantidad en Cero
+*/
+DROP TABLE IF EXISTS `aux_venta`;
+CREATE TABLE IF NOT EXISTS `aux_venta` (
+  `IdVenta`				INTEGER,
+  `Fecha` 				DATE NOT NULL,
+  `Fecha_Entrega` 		DATE NOT NULL,
+  `IdCliente`			INTEGER, 
+  `IdSucursal`			INTEGER,
+  `IdEmpleado`			INTEGER,
+  `IdProducto`			INTEGER,
+  `Precio`				FLOAT,
+  `Cantidad`			INTEGER,
+  `Motivo`				INTEGER
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+UPDATE venta SET Cantidad = REPLACE(Cantidad, '\r', '');
+
+INSERT INTO aux_venta (IdVenta, Fecha, Fecha_Entrega, IdCliente, IdSucursal, IdEmpleado, IdProducto, Precio, Cantidad, Motivo)
+SELECT IdVenta, Fecha, Fecha_Entrega, IdCliente, IdSucursal, IdEmpleado, IdProducto, Precio, 0, 1
+FROM venta WHERE Cantidad = '' or Cantidad is null;
+
+UPDATE venta SET Cantidad = '1' WHERE Cantidad = '' or Cantidad is null;
+ALTER TABLE `venta` CHANGE `Cantidad` `Cantidad` INTEGER NOT NULL DEFAULT '0';
+
+/*Chequeo de claves duplicadas*/
+SELECT IdCliente, COUNT(*) FROM cliente GROUP BY IdCliente HAVING COUNT(*) > 1;
+SELECT IdSucursal, COUNT(*) FROM sucursal GROUP BY IdSucursal HAVING COUNT(*) > 1;
+SELECT IdEmpleado, COUNT(*) FROM empleado GROUP BY IdEmpleado HAVING COUNT(*) > 1;
+SELECT IdProveedor, COUNT(*) FROM proveedor GROUP BY IdProveedor HAVING COUNT(*) > 1;
+SELECT IdProducto, COUNT(*) FROM producto GROUP BY IdProducto HAVING COUNT(*) > 1;
+
+SELECT Sucursal, IdEmpleado, COUNT(*) 
+FROM empleado 
+GROUP BY Sucursal, IdEmpleado 
+HAVING COUNT(*) > 1;
+
+select * from empleado where IdEmpleado in (3875,3144);
+-- select count(*) from empleado;
+
+SELECT e.*, s.IdSucursal
+FROM empleado e JOIN sucursal s
+	ON (e.Sucursal = s.Sucursal);
+    
+select distinct Sucursal from empleado
+where Sucursal NOT IN (select Sucursal from sucursal);
+
+/*Generacion de clave única tabla empleado mediante creacion de clave subrogada*/
+UPDATE empleado SET Sucursal = 'Mendoza1' WHERE Sucursal = 'Mendoza 1';
+UPDATE empleado SET Sucursal = 'Mendoza2' WHERE Sucursal = 'Mendoza 2';
+-- UPDATE empleado SET Sucursal = 'Córdoba Quiroz' WHERE Sucursal = 'Cordoba Quiroz';
+
+ALTER TABLE `empleado` ADD `IdSucursal` INT NULL DEFAULT '0' AFTER `Sucursal`;
+
+UPDATE empleado e JOIN sucursal s
+	ON (e.Sucursal = s.Sucursal)
+SET e.IdSucursal = s.IdSucursal;
+
+ALTER TABLE `empleado` DROP `Sucursal`;
+
+ALTER TABLE `empleado` ADD `CodigoEmpleado` INT NULL DEFAULT '0' AFTER `IdEmpleado`;
+
+UPDATE empleado SET CodigoEmpleado = IdEmpleado;
+UPDATE empleado SET IdEmpleado = (IdSucursal * 1000000) + CodigoEmpleado;
+
+/*Chequeo de claves duplicadas*/
+SELECT * FROM `empleado`;
+SELECT IdEmpleado, COUNT(*) FROM empleado GROUP BY IdEmpleado HAVING COUNT(*) > 1;
+
+/*Modificacion de la clave foranea de empleado en venta*/
+UPDATE venta SET IdEmpleado = (IdSucursal * 1000000) + IdEmpleado;
+
+/*Normalizacion tabla empleado*/
+DROP TABLE IF EXISTS `cargo`;
+CREATE TABLE IF NOT EXISTS `cargo` (
+  `IdCargo` integer NOT NULL AUTO_INCREMENT,
+  `Cargo` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdCargo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+DROP TABLE IF EXISTS `sector`;
+CREATE TABLE IF NOT EXISTS `sector` (
+  `IdSector` integer NOT NULL AUTO_INCREMENT,
+  `Sector` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdSector`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO cargo (Cargo) SELECT DISTINCT Cargo FROM empleado ORDER BY 1;
+INSERT INTO sector (Sector) SELECT DISTINCT Sector FROM empleado ORDER BY 1;
+                    
+select * from cargo;
+select * from sector;
+
+ALTER TABLE `empleado` 	ADD `IdSector` INT NOT NULL DEFAULT '0' AFTER `IdSucursal`, 
+						ADD `IdCargo` INT NOT NULL DEFAULT '0' AFTER `IdSector`;
+
+UPDATE empleado e JOIN cargo c ON (c.Cargo = e.Cargo) SET e.IdCargo = c.IdCargo;
+UPDATE empleado e JOIN sector s ON (s.Sector = e.Sector) SET e.IdSector = s.IdSector;
+
+ALTER TABLE `empleado` DROP `Cargo`;
+ALTER TABLE `empleado` DROP `Sector`;
+
+SELECT * FROM `empleado`;
+
+UPDATE producto p JOIN tipo_producto t ON (p.Tipo = t.TipoProducto) SET p.IdTipoProducto = t.IdTipoProducto;
+
+SELECT * FROM `producto`;
+SELECT * FROM `tipo_producto`;
+
+ALTER TABLE `producto` DROP `Tipo`;
+ALTER TABLE `producto` ADD `CodigoEmpleado` INT NULL DEFAULT '0' AFTER `IdEmpleado`;
+
+select * from cliente;
